7b14d859d29d5a0cd65d609014c384f44055dff0
Abel ORian <abel.orian@gmail.com>
2016-06-09 02:44:29 -0400

    Add: city model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609060328) do
+ActiveRecord::Schema.define(version: 20160609064313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "country_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "cities", ["country_id"], name: "index_cities_on_country_id", using: :btree
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 20160609060328) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cities", "countries"
 end
